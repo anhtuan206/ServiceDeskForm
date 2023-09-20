3f22b1152f184ea0e746ffdcdc37a61e8b276fdd
anhtuan206 <anhtuan206@gmail.com>
2023-09-20 19:52:50 +0700

    cập nhật phiên bản phần mềm

--- ServiceDeskForm/application/views/servicedesk/view_servicedesk.py
+++ ServiceDeskForm/application/views/servicedesk/view_servicedesk.py
@@ -12,7 +12,7 @@ urllib3.disable_warnings()
 bp_rootview = Blueprint("rootview",__name__,url_prefix="/")
 @bp_rootview.get("/")
 def rootview():
-    return "<h1>Xin chào, đây là quy trình triển khai phần mềm tự động CICD</h1>"
+    return "<h1>Xin chào, đây là quy trình triển khai phần mềm tự động CICD</h1><br><h4>Cập nhật phiên bản phẩn mềm.</h4>"
 
 bp_servicedesk = Blueprint("servicedesk", __name__, url_prefix="/servicedesk")
 
